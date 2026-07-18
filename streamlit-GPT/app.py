f5eea9995831e43093d5b84ba609ff848b5699d8
Aryan <pardhiaryan957@gmail.com>
2026-07-18 14:49:51 +0530

    Fix variable initialization name to OllamaLLM

--- streamlit-GPT/app.py
+++ streamlit-GPT/app.py
@@ -1,4 +1,4 @@
-from langchain_community.llms import Ollama
+from langchain_ollama import OllamaLLM
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -16,7 +16,7 @@ st.title("My GPT")
 input_text = st.text_input("What question do you have in mind?")
 
 # Ollama
-llm = Ollama(model="gemma2:2b")
+llm = OllamaLLM(model="gemma2:2b")
 
 output_parser = StrOutputParser()
 
